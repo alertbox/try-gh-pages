e5d1bed4e6daabc0ad5c450a4edccb1d69daa916
It's actually me <kosala.nuwan@gmail.com>
2022-08-21 17:25:57 +0000

    chore: improve logs

--- .devcontainer/post-create.zsh
+++ .devcontainer/post-create.zsh
@@ -2,23 +2,22 @@
 
 ## Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+  cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 fi
 
 # If there's a Gemfile, then run `bundle install`
 # It's assumed that the Gemfile will install Jekyll too
 if [ -f Gemfile ]; then
-    echo Gem file found. Installing ...
-    bundle install &>/dev/null;
+  echo Gemfile found.
+  bundle install &> /dev/null;
+else
+  echo Gemfile does not exist. &> /dev/null;
 fi
 
 # Install latest jekyll and github-pages for older jekyll
 if (($+commands[jekyll])); then 
-  echo Jekyll exists. &>/dev/null;
-# fi
-# if command -v jekyll >/dev/null; then
-#   echo jekyll exists
+  echo Jekyll exists. &> /dev/null;
 else
-  echo Jekyll does not exist. Installing ...
-  gem install jekyll github-pages &>/dev/null;
+  echo Jekyll does not exist.
+  gem install jekyll github-pages &> /dev/null;
 fi
