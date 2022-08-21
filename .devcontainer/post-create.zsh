703cebb725259fdf61f2b728c97a9d21cc778285
It's actually me <kosala.nuwan@gmail.com>
2022-08-21 13:36:16 +0000

    fix: gem install not installing jekyll

--- .devcontainer/post-create.zsh
+++ .devcontainer/post-create.zsh
@@ -2,8 +2,7 @@
 
 ## Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs \
-        gem install bundler jekyll github-pages -v
+    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler jekyll github-pages -v
 fi
 
 # If there's a Gemfile, then run `bundle install`
