ef1f4de74f62816adcad0643b0d29bb7d2230195
It's actually me <kosala.nuwan@gmail.com>
2022-08-21 13:23:32 +0000

    chore: add jekyll and gh-pages via post-script

--- .devcontainer/post-create.zsh
+++ .devcontainer/post-create.zsh
@@ -2,7 +2,8 @@
 
 ## Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs \
+        gem install bundler jekyll github-pages -v
 fi
 
 # If there's a Gemfile, then run `bundle install`
